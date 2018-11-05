85e97d8f085d3304dbf147251a19f2d8b9484ecc
John Buizon <donjuanbuizon@gmail.com>
2018-11-05 14:27:26 +1100

    Shooting

--- Assessment 2/Assets/Scripts/Player.cs
+++ Assessment 2/Assets/Scripts/Player.cs
@@ -8,6 +8,13 @@ public class Player : MonoBehaviour
     public float movementSpeed;
     public GameObject camera;
 
+    public GameObject playerObj;
+
+    public GameObject projectileSpawnPoint;
+    public float waitTime;
+    public GameObject projectile;
+
+
     // Methods
     void Update()
     {
@@ -23,12 +30,32 @@ public class Player : MonoBehaviour
             Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
             targetRotation.x = 0;
             targetRotation.z = 0;
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 7f * Time.deltaTime);
+            playerObj.transform.rotation = Quaternion.Slerp(playerObj.transform.rotation, targetRotation, 7f * Time.deltaTime);
         }
 
         //Player Movement
         if (Input.GetKey(KeyCode.W))
             transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * movementSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+
+
+        //Shooting
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(projectile.transform, projectileSpawnPoint.transform.position, Quaternion.identity);
     }
 
 }
---
+++ Assessment 2/Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour 
+{
+    public float speed;
+    public float maxDistance;
+
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        maxDistance += 1 * Time.deltaTime;
+
+        if(maxDistance >= 5)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
